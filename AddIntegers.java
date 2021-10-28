0dd36f09878fcae644b9ef4109fd7ba5d725cb7b
belkasri12 <belkasri@umich.edu>
2021-10-28 18:34:37 -0400

    jenkins build #2

--- AddIntegers.java
+++ AddIntegers.java
@@ -3,8 +3,8 @@ public class AddIntegers {
     public static void main(String[] args) {
 	// CHANGED
 
-        int first = 30;
-        int second = 35;
+        int first = 40;
+        int second = 45;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
